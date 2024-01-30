d2c4cede115e82fc81cc61268bcff2bf48dd917a
64bit-polygon <64bit.polygon@gmail.com>
2024-01-30 17:09:56 -0500

    fix: added breakpoints, updated contact mobile styles

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -5,8 +5,8 @@ const Contact = () => (
   <address className={styles.contact}>
     <div>Nate De La Cruz</div>
     <div>NYC Based</div>
-    <a href="mailto:64bit.polygon@gmail.com">64bit.polygon@gmail.com</a>
-    <a href="https://github.com/64bit-polygon/links" target="_blank">github.com/64bit-polygon</a>
+    <a href="mailto:64bit.polygon@gmail.com">64bit.<wbr/>polygon<wbr/>@gmail.com</a>
+    <a href="https://github.com/64bit-polygon/links" target="_blank">github.com/<wbr/>64bit-polygon</a>
     <div>😘</div>
   </address>
 );
